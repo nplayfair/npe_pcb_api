7faf40e52fbc8e872e5ddbde34cc9a57197404bf
Nick Playfair <nick@playfair.dev>
2020-12-29 22:02:08 +0000

    Use ejs for views

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const Koa       = require('koa');
+const Koa       = require('koa')
 const KoaRouter = require('koa-router')
-const json      = require('koa-json');
+const json      = require('koa-json')
+const render    = require('koa-ejs')
+const path      = require('path')
 
 const app     = new Koa();
 const router  = new KoaRouter();
 
 // JSON Prettier middleware
 app.use(json());
+
+// Use ejs renderer
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+// Index route
+router.get('/', async ctx => {
+  await ctx.render('index');
+})
 
 router.get('/test', ctx => (ctx.body = { message: "Hello" }));
 
