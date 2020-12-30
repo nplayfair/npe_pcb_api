7cef12f758db1ec9e117dfc6a2edec6ce903dada
Nick Playfair <nick@playfair.dev>
2020-12-30 00:21:07 +0000

    Use dotenv

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const json        = require('koa-json')
 const render      = require('koa-ejs')
 const bodyParser  = require('koa-bodyparser')
 const path        = require('path')
+require('dotenv').config()
 
 const app     = new Koa();
 const router  = new KoaRouter();
