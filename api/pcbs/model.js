d59f8c3cc0e1a3dd141668e0a489be8260100010
Nick Playfair <nick@playfair.dev>
2021-01-05 12:31:24 +0000

    Remove unnecessary layers of abstraction

--- api/pcbs/model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../../models'); 
-
-exports.readAll = async function () {
-  return db.Pcb.findAll({ raw: true })
-}
-
-exports.readOne = async function (prodCode) {
-  return db.Pcb.findOne({ where: { productCode: prodCode } })
-}
